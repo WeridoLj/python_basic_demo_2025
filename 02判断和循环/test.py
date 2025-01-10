2c7f59b81377c54817e0196bcaa725629e6d673a
陆建 <lujian@lujiandeMacBook-Pro.local>
2025-01-10 18:30:32 +0800

    1

--- 02判断和循环/test.py
+++ 02判断和循环/test.py
@@ -13,7 +13,7 @@ except ValueError:
     print("分数请输入数字")
 
 
-#任务1：使用for循环打印乘法表（九九表）
+#任务2：使用for循环打印乘法表（九九表）
 """
 示例：
 1 * 1 = 1
@@ -21,5 +21,13 @@ except ValueError:
 ...
 """
 for i in range(1,10):
-    res = [i * j for i in range(1,10) for j in range(1,10)]
+    for j in range(1,i+1):
+        res = i * j
+        print(f"{i} * {j} = {res}", end="\t")
+    print()
 
+
+#任务3：输入一个整数，输出从1到这个整数中所有奇数的平方，并存储到列表中（使用列表推导式）
+num1 = 19
+num2 = [x ** 2 for x in range(1, num1) if x %2 != 0]
+print(num2)
